e10c2288a2eb09fceba1ef598c677c5894c8fc7e
PaRaG <paragd001@gmail.com>
2019-10-18 17:28:23 +0530

    Rename

--- src/test/java/bindings/Gmail_Page.java
+++ src/test/java/bindings/Gmail_Page.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
 
 
-public class Basket extends Reuseable {
+public class Gmail_Page extends Reuseable {
      WebDriver driver;
 
 
